442acfbf00834c3e9963f760b8fec382635fa676
DaveZombee <20luky301@gmail.com>
2023-10-30 12:44:52 -0400

    Create edit_routine use case files

---
+++ src/use_case/edit_routine/change_order/ChangeOrderInteractor.java
@@ -0,0 +1,4 @@
+package use_case.edit_routine.change_order;
+
+public class ChangeOrderInteractor {
+}
---
+++ src/use_case/edit_routine/change_reps/ChangeRepsInteractor.java
@@ -0,0 +1,4 @@
+package use_case.edit_routine.change_reps;
+
+public class ChangeRepsInteractor {
+}
