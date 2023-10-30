9a0a02126a5122e7575d07a92961d01553815c7a
dhwanikj <dhwani.jain@mail.utoronto.ca>
2023-10-30 13:00:32 -0400

    entitiy'

--- src/entity/Routine.java
+++ src/entity/Routine.java
@@ -1,4 +1,12 @@
 package entity;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 public class Routine {
+    private LinkedHashMap<String, Integer> exercises;
+
+    public Routine(LinkedHashMap exercises) {
+        this.exercises = exercises;
+    }
 }
