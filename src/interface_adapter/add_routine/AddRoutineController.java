3beb6cf902f7eadbe87f28216d759f55b70a9265
cindxyc <cindxyc@cindxycs-MacBook-Pro.local>
2023-10-30 11:36:45 -0400

    edit to AddRoutineController

--- src/interface_adapter/add_routine/AddRoutineController.java
+++ src/interface_adapter/add_routine/AddRoutineController.java
@@ -1,4 +1,11 @@
 package interface_adapter.add_routine;
 
+import use_case.add_routine.AddRoutineInputBoundary;
+
 public class AddRoutineController {
+
+    final AddRoutineInputBoundary addRoutineUseCaseInteractor;
+    public AddRoutineController(AddRoutineInputBoundary addRoutineUseCaseInteractor) {
+        this.addRoutineUseCaseInteractor = addRoutineUseCaseInteractor;
+    }
 }
