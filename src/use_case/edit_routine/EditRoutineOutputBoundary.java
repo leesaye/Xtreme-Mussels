214452e092bd4c26fcd0cd65d03d5c9f01265565
DaveZombee <20luky301@gmail.com>
2023-11-04 15:56:11 -0400

    Implement methods for EditRoutine output

--- src/use_case/edit_routine/EditRoutineOutputBoundary.java
+++ src/use_case/edit_routine/EditRoutineOutputBoundary.java
@@ -1,4 +1,7 @@
 package use_case.edit_routine;
 
 public interface EditRoutineOutputBoundary {
+    void prepareSuccessView(EditRoutineOutputData data);
+
+    void prepareFailView(String error);
 }
--- src/use_case/edit_routine/EditRoutineOutputData.java
+++ src/use_case/edit_routine/EditRoutineOutputData.java
@@ -1,4 +1,12 @@
 package use_case.edit_routine;
 
 public class EditRoutineOutputData {
+    private final String name; // display message that says eg "Edits to <name> saved"
+    public EditRoutineOutputData(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
