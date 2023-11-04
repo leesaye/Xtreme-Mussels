206bfe439e8a4449341f46beb83ba3714efa018c
DaveZombee <20luky301@gmail.com>
2023-11-04 15:46:50 -0400

    Write EditRoutine input files

--- src/use_case/edit_routine/EditRoutineInputBoundary.java
+++ src/use_case/edit_routine/EditRoutineInputBoundary.java
@@ -1,4 +1,5 @@
 package use_case.edit_routine;
 
 public interface EditRoutineInputBoundary {
+    void execute(EditRoutineInputData editInputData);
 }
--- src/use_case/edit_routine/EditRoutineInputData.java
+++ src/use_case/edit_routine/EditRoutineInputData.java
@@ -1,4 +1,40 @@
 package use_case.edit_routine;
 
+import entity.Exercise;
+
+import java.util.ArrayList;
+
 public class EditRoutineInputData {
+
+    final private String name;
+
+    final private ArrayList<Integer> reps;
+
+    final private ArrayList<Integer> sets;
+
+    final private ArrayList<Exercise>  exercises;
+
+    public EditRoutineInputData(String name, ArrayList<Integer> reps,
+                                ArrayList<Integer> sets, ArrayList<Exercise> exercises ) {
+        this.name = name;
+        this.reps = reps;
+        this.sets = sets;
+        this.exercises = exercises;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Integer> getReps() {
+        return reps;
+    }
+
+    public ArrayList<Integer> getSets() {
+        return sets;
+    }
+
+    public ArrayList<Exercise> getExercises() {
+        return exercises;
+    }
 }
