ea5500d7086ab08103b8a2891c351c346da1f181
cindxyc <chencindy.1206@gmail.com>
2023-11-08 21:51:59 -0500

    ignore

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,15 +1,7 @@
 package app;
 
-import interface_adapter.add_routine.AddRoutineViewModel;
-import view.AddRoutineView;
-
 class Main {
     public static void main(String[] args) {
-
-        AddRoutineViewModel addRoutineViewModel = new AddRoutineViewModel();
-
-        // AddRoutineView signupView = AddRoutineUseCaseFactory.create();
-
     }
 
 }
