3bef08213944052e8fbbb1fc86da7202c365b598
cindxyc <chencindy.1206@gmail.com>
2023-11-13 12:29:05 -0500

    implemented AddRoutineController

--- src/entity/Routine.java
+++ src/entity/Routine.java
@@ -1,21 +1,26 @@
 package entity;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Routine {
+    private final int id;
     private final String routineName;  // routine name cannot be repeated?
-    private final ArrayList routine;  // list of exercises
+    private final ArrayList<Map<String, Object>> exercisesList;  // list of exercises
 
 
-    public Routine(String routineName, ArrayList routine) {
+    public Routine(int id, String routineName, ArrayList<Map<String, Object>> exercisesList) {
+        this.id = id;
         this.routineName = routineName;
-        this.routine = routine;
+        this.exercisesList = exercisesList;
     }
 
+    public int getId() {
+        return id;
+    }
     public String getRoutineName() {
         return routineName;
     }
-    public ArrayList getRoutine() {
-        return routine;
+    public ArrayList<Map<String, Object>> getExercisesList() {
+        return exercisesList;
     }
 }
