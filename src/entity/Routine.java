35547a77420d4df04fc4220994f254f8816e248a
cindxyc <chencindy.1206@gmail.com>
2023-11-13 11:58:24 -0500

    added routine entity

--- src/entity/Routine.java
+++ src/entity/Routine.java
@@ -1,4 +1,21 @@
 package entity;
 
+import java.util.ArrayList;
+
 public class Routine {
+    private final String routineName;  // routine name cannot be repeated?
+    private final ArrayList routine;  // list of exercises
+
+
+    public Routine(String routineName, ArrayList routine) {
+        this.routineName = routineName;
+        this.routine = routine;
+    }
+
+    public String getRoutineName() {
+        return routineName;
+    }
+    public ArrayList getRoutine() {
+        return routine;
+    }
 }
