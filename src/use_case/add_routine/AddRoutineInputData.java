4a6294a847dc698935305bca2cf83d4b4273266d
cindxyc <chencindy.1206@gmail.com>
2023-11-13 12:34:37 -0500

    implemented AddRoutineController

--- src/use_case/add_routine/AddRoutineInputData.java
+++ src/use_case/add_routine/AddRoutineInputData.java
@@ -1,4 +1,25 @@
 package use_case.add_routine;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class AddRoutineInputData {
+    private final int id;
+    private final String routineName;
+    private final ArrayList<Map<String, Object>> exercisesList;
+    public AddRoutineInputData(int id, String routineName, ArrayList<Map<String, Object>> exercisesList) {
+        this.id = id;
+        this.routineName = routineName;
+        this.exercisesList = exercisesList;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getRoutineName() {
+        return routineName;
+    }
+    public ArrayList<Map<String, Object>> getExercisesList() {
+        return exercisesList;
+    }
 }
