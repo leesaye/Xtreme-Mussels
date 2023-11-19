98b3fa7673f9b811db561114d61ae5decc873f03
DaveZombee <20luky301@gmail.com>
2023-11-19 15:28:45 -0500

    Create constructor and execute for reorder controller

--- src/interface_adapter/reorder_routine/ReorderRoutineController.java
+++ src/interface_adapter/reorder_routine/ReorderRoutineController.java
@@ -1,4 +1,22 @@
 package interface_adapter.reorder_routine;
 
+import use_case.reorder_routine.ReorderRoutineInputBoundary;
+import entity.Exercise
+import use_case.reorder_routine.ReorderRoutineInputData;
+
+import java.util.ArrayList;
+
 public class ReorderRoutineController {
+
+    final ReorderRoutineInputBoundary reorderUseCaseInteractor;
+
+    public ReorderRoutineController(ReorderRoutineInputBoundary reorderUseCaseInteractor) {
+        this.reorderUseCaseInteractor = reorderUseCaseInteractor;
+    }
+
+    public void execute(int id, ArrayList<Exercise> exercises) {
+        ReorderRoutineInputData reorderInputData = new ReorderRoutineInputData(id, exercises);
+
+        reorderUseCaseInteractor.execute(reorderInputData);
+    }
 }
