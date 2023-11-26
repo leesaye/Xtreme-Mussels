b2a49109a81335f0b3ab15a5dabfc5def421bedd
DaveZombee <20luky301@gmail.com>
2023-11-25 23:38:22 -0500

    Create rename_routine files

---
+++ src/interface_adapter/rename_routine/RenameRoutineViewModel.java
@@ -0,0 +1,4 @@
+package interface_adapter.rename_routine;
+
+public class RenameRoutineViewModel {
+}
---
+++ src/use_case/rename_routine/RenameRoutineOutputBoundary.java
@@ -0,0 +1,4 @@
+package use_case.rename_routine;
+
+public interface RenameRoutineOutputBoundary {
+}
