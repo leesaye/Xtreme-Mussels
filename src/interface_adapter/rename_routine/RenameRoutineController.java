95aa763861e20ff152d6590bd10416dadac37723
DaveZombee <20luky301@gmail.com>
2023-11-25 23:59:45 -0500

    Create methods for interface adapters

--- src/interface_adapter/rename_routine/RenameRoutineController.java
+++ src/interface_adapter/rename_routine/RenameRoutineController.java
@@ -1,4 +1,19 @@
 package interface_adapter.rename_routine;
 
+import use_case.rename_routine.RenameRoutineInputBoundary;
+import use_case.rename_routine.RenameRoutineInputData;
+
 public class RenameRoutineController {
+
+    final RenameRoutineInputBoundary renameRoutineInteractor;
+
+    public RenameRoutineController(RenameRoutineInputBoundary renameRoutineInteractor) {
+        this.renameRoutineInteractor = renameRoutineInteractor;
+    }
+
+    public void execute(int id, String name) {
+        RenameRoutineInputData renameRoutineInputData = new RenameRoutineInputData(id, name);
+
+        renameRoutineInteractor.execute(renameRoutineInputData);
+    }
 }
--- src/interface_adapter/rename_routine/RenameRoutineViewModel.java
+++ src/interface_adapter/rename_routine/RenameRoutineViewModel.java
@@ -1,4 +1,30 @@
 package interface_adapter.rename_routine;
 
-public class RenameRoutineViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class RenameRoutineViewModel extends ViewModel {
+
+    private static RenameRoutineState state = new RenameRoutineState();
+
+    public RenameRoutineViewModel() { super("rename")};
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public static RenameRoutineState getState() {
+        return state;
+    }
 }
