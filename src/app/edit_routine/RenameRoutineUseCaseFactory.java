073f39c0e93b61fefb03a31ac5be618bb6d89525
DaveZombee <20luky301@gmail.com>
2023-11-26 00:06:15 -0500

    Create rename routine use case factory

--- src/app/edit_routine/RenameRoutineUseCaseFactory.java
+++ src/app/edit_routine/RenameRoutineUseCaseFactory.java
@@ -1,4 +1,37 @@
 package app.edit_routine;
 
+import data_access.FileRoutineDataAccessObject;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.rename_routine.RenameRoutineController;
+import interface_adapter.rename_routine.RenameRoutinePresenter;
+import interface_adapter.rename_routine.RenameRoutineViewModel;
+import use_case.rename_routine.RenameRoutineInputBoundary;
+import use_case.rename_routine.RenameRoutineInteractor;
+import use_case.rename_routine.RenameRoutineOutputBoundary;
+
 public class RenameRoutineUseCaseFactory {
+
+    private RenameRoutineUseCaseFactory() {}
+
+    public static RenameRoutineView create(
+            ViewManagerModel viewManagerModel, RenameRoutineViewModel renameRoutineRepViewModel,
+            FileRoutineDataAccessObject routineDataAccessObject) {
+        // TODO: Uncomment when data access has been written
+//        try {
+        RenameRoutineController renameRoutineController = createRenameRoutineRepUseCase(viewManagerModel, renameRoutineRepViewModel, routineDataAccessObject);
+        return new RenameRoutineView(renameRoutineController, renameRoutineRepViewModel);
+//        } catch (IOException e) {
+//            JOptionPane.showMessageDialog(null, "Could not open routine data file.");
+//        }
+    }
+
+    private static RenameRoutineController createRenameRoutineRepUseCase(ViewManagerModel viewManagerModel,
+                                                                         RenameRoutineViewModel renameRoutineRepViewModel,
+                                                                         FileRoutineDataAccessObject routineDataAccessObject) {
+        RenameRoutineOutputBoundary renameRoutineOutputBoundary = new RenameRoutinePresenter(viewManagerModel, renameRoutineRepViewModel);
+
+        RenameRoutineInputBoundary renameInteractor = new RenameRoutineInteractor(routineDataAccessObject, renameRoutineOutputBoundary);
+
+        return new RenameRoutineController(renameInteractor);
+    }
 }
