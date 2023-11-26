334245aca9a1502b5d2fa55612326d945d8f6f33
DaveZombee <20luky301@gmail.com>
2023-11-25 23:59:59 -0500

    Create methods and interfaces for use case classes

--- src/use_case/rename_routine/RenameRoutineInputBoundary.java
+++ src/use_case/rename_routine/RenameRoutineInputBoundary.java
@@ -1,4 +1,5 @@
 package use_case.rename_routine;
 
 public interface RenameRoutineInputBoundary {
+    void execute(RenameRoutineInputData renameRoutineInputData);
 }
--- src/use_case/rename_routine/RenameRoutineOutputData.java
+++ src/use_case/rename_routine/RenameRoutineOutputData.java
@@ -1,4 +1,14 @@
 package use_case.rename_routine;
 
 public class RenameRoutineOutputData {
+
+    private final String name;
+
+    public RenameRoutineOutputData(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
