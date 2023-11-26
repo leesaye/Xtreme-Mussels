b30aecfa713190783f1b3bdc9e7ecf9ebaf0655b
DaveZombee <20luky301@gmail.com>
2023-11-26 00:00:18 -0500

    Add new method for rename routine

--- src/data_access/FileRoutineDataAccessObject.java
+++ src/data_access/FileRoutineDataAccessObject.java
@@ -1,14 +1,19 @@
 package data_access;
 
-import entity.Exercise;
 import use_case.adjust_setrep.AdjustSetRepDataAccessInterface;
+import use_case.rename_routine.RenameRoutineDataAccessInterface;
 
 import java.util.ArrayList;
 
-public class FileRoutineDataAccessObject implements AdjustSetRepDataAccessInterface {
+public class FileRoutineDataAccessObject implements AdjustSetRepDataAccessInterface, RenameRoutineDataAccessInterface {
     @Override
     public boolean existsById(int id) {
         return false;
+    }
+
+    @Override
+    public void changeName(int id, String name) {
+
     }
 
     @Override
