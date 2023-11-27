b4928dacea2063bf308d4dcbcc73ff10eea7a917
29-sp <srika.ponnavolu@mail.utoronto.ca>
2023-11-27 10:30:55 -0500

    corrected OutputDataBoundary

--- src/use_case/delete_exercise/DeleteExerciseOutputData.java
+++ src/use_case/delete_exercise/DeleteExerciseOutputData.java
@@ -1,15 +1,21 @@
 package use_case.delete_exercise;
 
+import entity.Exercise;
+
+import java.util.ArrayList;
+
 public class DeleteExerciseOutputData {
     /* is output data for this use case the updated routine or something more like
      * a success message like the other edit-related use case? **/
 
     private final int id;
     private final String name; // message: Added <name> to routine id
-    public DeleteExerciseOutputData(int id, String name) {
+    private final ArrayList<Exercise> exercises;
+    public DeleteExerciseOutputData(int id, String name, ArrayList<Exercise> exercises) {
 
         this.id = id;
         this.name = name;
+        this.exercises = exercises;
     }
 
     public String getName() {
@@ -18,5 +24,9 @@ public class DeleteExerciseOutputData {
 
     public int getId() {
         return id;
+    }
+
+    public ArrayList<Exercise> getExercises() {
+        return exercises;
     }
 }
