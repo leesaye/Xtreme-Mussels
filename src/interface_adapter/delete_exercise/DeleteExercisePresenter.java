86b3e9df9abf37e6222077c042c346c868c7d195
29-sp <srika.ponnavolu@mail.utoronto.ca>
2023-11-27 10:34:17 -0500

    implemented interface_adapter for delete exercise use case

--- src/interface_adapter/delete_exercise/DeleteExercisePresenter.java
+++ src/interface_adapter/delete_exercise/DeleteExercisePresenter.java
@@ -1,5 +1,33 @@
 package interface_adapter.delete_exercise;
 
-public class DeleteExercisePresenter {
+import interface_adapter.ViewManagerModel;
+import interface_adapter.delete_exercise.DeleteExerciseState;
+import interface_adapter.delete_exercise.DeleteExerciseViewModel;
+import use_case.delete_exercise.DeleteExerciseOutputBoundary;
+import use_case.delete_exercise.DeleteExerciseOutputData;
+
+public class DeleteExercisePresenter implements DeleteExerciseOutputBoundary {
+    private final DeleteExerciseViewModel deleteExerciseViewModel;
+
+    private ViewManagerModel viewManagerModel;
+
+    public DeleteExercisePresenter(ViewManagerModel viewManagerModel, DeleteExerciseViewModel deleteExerciseViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.deleteExerciseViewModel = deleteExerciseViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(DeleteExerciseOutputData data) {
+        DeleteExerciseState deleteExerciseState = deleteExerciseViewModel.getState();
+        deleteExerciseState.setExercises(data.getExercises());
+        deleteExerciseViewModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        DeleteExerciseState deleteExerciseState = deleteExerciseViewModel.getState();
+        deleteExerciseState.deleteExerciseNameError(error);
+        deleteExerciseViewModel.firePropertyChanged();
+    }
 
 }
--- src/interface_adapter/delete_exercise/DeleteExerciseState.java
+++ src/interface_adapter/delete_exercise/DeleteExerciseState.java
@@ -1,5 +1,43 @@
 package interface_adapter.delete_exercise;
 
+import entity.Exercise;
+
+import java.util.ArrayList;
+
 public class DeleteExerciseState {
+    private String name;
+
+    private int id;
+
+    private ArrayList<Exercise> exercises = null;
+
+    private String nameError;
+
+    public DeleteExerciseState() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setExercises(ArrayList<Exercise> exercises) {
+        this.exercises = exercises;
+    }
+
+    public ArrayList<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public void deleteExerciseNameError(String nameError) {
+        this.nameError = nameError;
+    }
+
+    public String getRoutineNameError() {
+        return nameError;
+    }
 
 }
