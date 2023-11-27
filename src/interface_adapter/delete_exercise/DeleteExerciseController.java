813e9a9b304c07bd732a8d8588e94cf8adaffb9b
29-sp <srika.ponnavolu@mail.utoronto.ca>
2023-11-27 08:33:27 -0500

    implemented DeleteExerciseController

--- src/interface_adapter/delete_exercise/DeleteExerciseController.java
+++ src/interface_adapter/delete_exercise/DeleteExerciseController.java
@@ -1,5 +1,19 @@
 package interface_adapter.delete_exercise;
 
+import use_case.delete_exercise.DeleteExerciseInputBoundary;
+import use_case.delete_exercise.DeleteExerciseInputData;
+
 public class DeleteExerciseController{
+    final DeleteExerciseInputBoundary deleteExerciseUseCaseInteractor;
+
+    public DeleteExerciseController(DeleteExerciseInputBoundary deleteExerciseUseCaseInteractor) {
+        this.deleteExerciseUseCaseInteractor = deleteExerciseUseCaseInteractor;
+    }
+
+    public void execute(int id, String name) {
+        //pass in data / exercise name?
+        DeleteExerciseInputData addExerciseInputData = new DeleteExerciseInputData(id, name);
+        deleteExerciseUseCaseInteractor.execute(addExerciseInputData);
+    }
 
 }
