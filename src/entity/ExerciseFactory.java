5f4548ef0e3add0542cc554127277efb123c94db
lisa <lis.ye@mail.utoronto.ca>
2023-11-27 23:09:55 -0500

    Created ExerciseFactory

--- src/entity/ExerciseFactory.java
+++ src/entity/ExerciseFactory.java
@@ -1,2 +1,9 @@
-package entity;public class ExerciseFactory {
+package entity;
+
+import java.util.ArrayList;
+
+public class ExerciseFactory {
+    public Exercise create(String name, String target, String equipment, ArrayList<String> instructions, String id, int sets, int reps) {
+        return new Exercise(name, target, equipment, instructions, id, sets, reps);
+    }
 }
