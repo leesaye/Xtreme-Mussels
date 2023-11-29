e938e488aba475cbf08d265ad0073fb967607e64
lisa <lis.ye@mail.utoronto.ca>
2023-11-29 18:52:35 -0500

    Created/completed interactor for look up routine

--- src/use_case/lookup_routine/LookUpRoutineInteractor.java
+++ src/use_case/lookup_routine/LookUpRoutineInteractor.java
@@ -1,2 +1,22 @@
-package use_case.lookup_routine;public class LookUpRoutineInteractor {
+package use_case.lookup_routine;
+
+public class LookUpRoutineInteractor implements LookUpRoutineInputBoundary {
+    final LookUpRoutineDataAccessInterface routineDataAccessObject;
+    final LookUpRoutineOutputBoundary routinePresenter;
+
+    public LookUpRoutineInteractor(LookUpRoutineDataAccessInterface routineDataAccessObject, LookUpRoutineOutputBoundary routinePresenter) {
+        this.routineDataAccessObject = routineDataAccessObject;
+        this.routinePresenter = routinePresenter;
+    }
+
+    @Override
+    public void execute(LookUpRoutineInputData lookUpRoutineInputData) {
+        if (routineDataAccessObject.existsByName(lookUpRoutineInputData.getRoutineName())) {
+            routinePresenter.prepareFailView("Invalid routine name.");
+        } else {
+            routineDataAccessObject.getRoutine(lookUpRoutineInputData.getRoutineName());
+            LookUpRoutineOutputData lookUpRoutineOutputData = new LookUpRoutineOutputData(routineDataAccessObject.getRoutine(lookUpRoutineInputData.getRoutineName()), false);
+            routinePresenter.prepareSuccessView(lookUpRoutineOutputData);
+        }
+    }
 }
