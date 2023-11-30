e2c5d297b013b4e9ea7d0bcc42c8e65eccb61133
lisa <lis.ye@mail.utoronto.ca>
2023-11-29 19:31:16 -0500

    Updates to interface adapters for look up routine

--- src/interface_adapter/lookup_routine/LookUpRoutinePresenter.java
+++ src/interface_adapter/lookup_routine/LookUpRoutinePresenter.java
@@ -1,4 +1,28 @@
 package interface_adapter.lookup_routine;
 
-public class LookUpRoutinePresenter {
+import use_case.lookup.LookUpOutputBoundary;
+import use_case.lookup.LookUpOutputData;
+
+public class LookUpRoutinePresenter implements LookUpOutputBoundary {
+    private final LookUpRoutineViewModel lookUpRoutineViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public LookUpRoutinePresenter(ViewManagerModel viewManagerModel, LookUpRoutineViewModel lookUpRoutineViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.lookUpRoutineViewModel = lookUpRoutineViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(LookUpOutputData response) {
+        LookUpRoutineState lookUpRoutineState = lookUpRoutineViewModel.getState();
+        // TODO
+        this.lookUpRoutineViewModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        LookUpRoutineState lookUpRoutineState = lookUpRoutineViewModel.getState();
+        lookUpRoutineState.setExercisesError(error);
+        lookUpRoutineViewModel.firePropertyChanged();
+    }
 }
--- src/interface_adapter/lookup_routine/LookUpRoutineState.java
+++ src/interface_adapter/lookup_routine/LookUpRoutineState.java
@@ -1,4 +1,60 @@
 package interface_adapter.lookup_routine;
 
+import entity.Exercise;
+import entity.Routine;
+
+import java.util.ArrayList;
+
 public class LookUpRoutineState {
+    private Routine routine = null;
+    private ArrayList<String> exercisesDisplay = null;
+    private String routineError = null;
+    private String exercisesDisplayError = null;
+
+    public LookUpRoutineState() {
+    }
+
+    public Routine getRoutine() {
+        return routine;
+    }
+
+    public String getRoutineError() {
+        return routineError;
+    }
+
+    public void setRoutine(Routine routine) {
+        this.routine = routine;
+    }
+
+    public void setRoutineError(String routineError) {
+        this.routineError = routineError;
+    }
+
+    public ArrayList<String> getExercisesDisplay() {
+        return exercisesDisplay;
+    }
+
+    public String getExercisesDisplayError() {
+        return exercisesDisplayError;
+    }
+
+    public void setExercisesDisplay(Routine routine) {
+        this.exercisesDisplay = this.toStringArray(routine.getExercisesList());
+    }
+
+    public void setExercisesDisplayError(String exercisesDisplayError) {
+        this.exercisesDisplayError = exercisesDisplayError;
+    }
+
+    public ArrayList<String> toStringArray(ArrayList<Exercise> exercise) {
+        ArrayList<String> display = new ArrayList<>();
+        for (Exercise value : exercise) {
+            display.add("Name: " + value.getName() +
+                    "\nEquipment: " + value.getEquipment() +
+                    "\nSets x Reps: " + value.getSets() + "x" + value.getReps() +
+                    "\nInstructions: " + value.getInstructions().toString());
+        }
+        return display;
+    }
+
 }
