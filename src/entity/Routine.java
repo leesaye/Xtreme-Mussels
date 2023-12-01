50fbb2323f0c92cebe4d7e1b827b453506f4d5a3
cindxyc <chencindy.1206@gmail.com>
2023-12-01 18:54:18 -0500

    deleted id, updated ExcerciseList type

--- src/entity/Routine.java
+++ src/entity/Routine.java
@@ -3,25 +3,18 @@ package entity;
 import java.util.*;
 
 public class Routine {
+    private String routineName;
+    private ArrayList<Exercise> exercisesList;
 
-    private final int id;
-    private final String routineName;  // routine name cannot be repeated?
-    private final ArrayList<Map<String, Object>> exercisesList;  // list of exercises
-
-
-    public Routine(int id, String routineName, ArrayList<Map<String, Object>> exercisesList) {
-        this.id = id;
+    public Routine(String routineName) {
         this.routineName = routineName;
-        this.exercisesList = exercisesList;
-
-    public int getId() {
-        return id;
+        this.exercisesList = new ArrayList<>();
     }
-      
+
     public String getRoutineName() {
         return routineName;
     }
-    public ArrayList<Map<String, Object>> getExercisesList() {
+    public ArrayList<Exercise> getExercisesList() {
         return exercisesList;
     }
 }
