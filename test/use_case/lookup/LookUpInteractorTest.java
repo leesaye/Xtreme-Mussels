ffc48dad04a39016fab9719152171e3d50c015a8
lisa <lis.ye@mail.utoronto.ca>
2023-12-01 15:00:02 -0500

    Added iml and updated a test

--- test/use_case/lookup/LookUpInteractorTest.java
+++ test/use_case/lookup/LookUpInteractorTest.java
@@ -1,4 +1,12 @@
+package use_case.lookup;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 class LookUpInteractorTest {
-  
+
+    @Test
+    void test() {
+
+    }
 }
