32657f8d8cded8338101153bffb26cb4cb4c1959
DaveZombee <20luky301@gmail.com>
2023-11-30 22:27:19 -0500

    Tests for AdjustSetRep and RenameRoutine

---
+++ test/interface_adapter/rename_routine/RenameRoutineControllerTest.java
@@ -0,0 +1,25 @@
+package interface_adapter.rename_routine;
+
+import org.junit.jupiter.api.Test;
+import use_case.rename_routine.RenameRoutineInputBoundary;
+import use_case.rename_routine.RenameRoutineInputData;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RenameRoutineControllerTest {
+
+    @Test
+    void test() {
+        RenameRoutineInputBoundary renameInteractor = new RenameRoutineInputBoundary() {
+
+            @Override
+            public void execute(RenameRoutineInputData renameRoutineInputData) {
+                assertEquals(1, renameRoutineInputData.getId());
+                assertEquals("old", renameRoutineInputData.getName());
+            }
+        };
+
+        RenameRoutineController controller = new RenameRoutineController(renameInteractor);
+
+        controller.execute(1, "old");
+    }
+}
