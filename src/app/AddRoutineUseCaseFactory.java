916ced516068ad6d81215968fa6b7174fd336f9b
DaveZombee <20luky301@gmail.com>
2023-12-02 17:16:59 -0500

    Delete things causing errors

--- src/app/AddRoutineUseCaseFactory.java
+++ src/app/AddRoutineUseCaseFactory.java
@@ -1,31 +1,9 @@
 package app;
 
-import entity.UserFactory;
-import interface_adapter.add_routine.AddRoutineController;
 import interface_adapter.add_routine.AddRoutineViewModel;
 import view.AddRoutineView;
-
-import java.io.IOException;
 
 public class AddRoutineUseCaseFactory {
     private AddRoutineUseCaseFactory() {}
 
-    public static AddRoutineView create(
-            AddRoutineViewModel addRoutineViewModel) {
-        return null;
-    }
-
-
-    // private static AddRoutineController createAddRoutineUseCase(
-    // AddRoutineViewModel addRoutineViewModel,
-
-        // Notice how we pass this method's parameters to the Presenter.
-    // LoginOutputBoundary loginOutputBoundary = new LoginPresenter(viewManagerModel, loggedInViewModel, loginViewModel);
-
-    // UserFactory userFactory = new CommonUserFactory();
-
-// LoginInputBoundary loginInteractor = new LoginInteractor(
-// userDataAccessObject, loginOutputBoundary);
-
-    // return new LoginController(loginInteractor);
 }
