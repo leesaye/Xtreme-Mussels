5e8b5ce3eaede11ec7bf54ecb9d26de7f8e76e8d
DaveZombee <20luky301@gmail.com>
2023-12-02 15:24:56 -0500

    commiting to stop merge problem

--- src/data_access/FileRoutineDataAccessObject.java
+++ src/data_access/FileRoutineDataAccessObject.java
@@ -1,11 +1,13 @@
 package data_access;
 
+import use_case.add_exercise.AddExerciseDataAccessInterface;
 import use_case.adjust_setrep.AdjustSetRepDataAccessInterface;
+import use_case.delete_exercise.DeleteExerciseDataAccessInterface;
 import use_case.rename_routine.RenameRoutineDataAccessInterface;
 
 import java.util.ArrayList;
 
-public class FileRoutineDataAccessObject implements AdjustSetRepDataAccessInterface, RenameRoutineDataAccessInterface {
+public class FileRoutineDataAccessObject implements AdjustSetRepDataAccessInterface, RenameRoutineDataAccessInterface, AddExerciseDataAccessInterface, DeleteExerciseDataAccessInterface {
     @Override
     public boolean existsById(int id) {
         return false;
