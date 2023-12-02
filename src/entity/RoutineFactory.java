4ce5e7edaed43dc8f2804de0a5cc9d65a88b9e12
cindxyc <chencindy.1206@gmail.com>
2023-12-02 11:54:01 -0500

    added Routine Factory

--- src/entity/RoutineFactory.java
+++ src/entity/RoutineFactory.java
@@ -1,2 +1,14 @@
-package entity;public class RoutineFactory {
+package entity;
+
+
+public class RoutineFactory {
+    /**
+     * Requires: routine name is valid.
+     *
+     * @param routineName
+     * @return
+     */
+    public Routine create(String routineName) {
+        return new Routine(routineName);
+    }
 }
