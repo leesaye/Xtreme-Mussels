ac8e9e2dc836fbd1d72f144bb51320580e703cde
lisa <lis.ye@mail.utoronto.ca>
2023-12-03 13:13:57 -0500

    Completed test for look up interface adapters

--- test/interface_adapter/lookup/LookUpControllerTest.java
+++ test/interface_adapter/lookup/LookUpControllerTest.java
@@ -1,2 +1,39 @@
-package interface_adapter.lookup;public class LookUpControllerTest {
+package interface_adapter.lookup;
+
+import org.junit.jupiter.api.Test;
+import use_case.lookup.LookUpInputBoundary;
+import use_case.lookup.LookUpInputData;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LookUpControllerTest {
+    @Test
+    void testTarget() {
+        LookUpInputBoundary interactor = new LookUpInputBoundary() {
+
+            @Override
+            public void execute(LookUpInputData lookUpInputData) {
+                assertEquals(lookUpInputData.getValue(), "hamstrings");
+                assertEquals(lookUpInputData.getQuery(), "target");
+            }
+        };
+
+        LookUpController controller = new LookUpController(interactor);
+        controller.execute("hamstrings", "target");
+    }
+
+    @Test
+    void testName() {
+        LookUpInputBoundary interactor = new LookUpInputBoundary() {
+
+            @Override
+            public void execute(LookUpInputData lookUpInputData) {
+                assertEquals(lookUpInputData.getValue(), "plank");
+                assertEquals(lookUpInputData.getQuery(), "name");
+            }
+        };
+
+        LookUpController controller = new LookUpController(interactor);
+        controller.execute("plank", "name");
+    }
 }
--- test/interface_adapter/lookup/LookUpPresenterTest.java
+++ test/interface_adapter/lookup/LookUpPresenterTest.java
@@ -1,2 +1,63 @@
-package interface_adapter.lookup;public class LookUpPresenterTest {
+package interface_adapter.lookup;
+
+import entity.Exercise;
+import entity.ExerciseFactory;
+import interface_adapter.ViewManagerModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import use_case.lookup.LookUpOutputBoundary;
+import use_case.lookup.LookUpOutputData;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LookUpPresenterTest {
+    ArrayList<Exercise> exercises = new ArrayList<>();
+
+    @BeforeEach
+    void init () {
+        ArrayList<String> instructions = new ArrayList<>();
+        instructions.add("test instructions");
+        exercises.add(ExerciseFactory.create("test name", "test target", "test equipment", instructions, "test id", 1, 2));
+    }
+
+    @Test
+    void prepareSuccessTest() {
+        LookUpViewModel lookUpViewModel = new LookUpViewModel("look up exercise") {
+            private LookUpState state = new LookUpState();
+
+            public LookUpState getState() { return state; }
+
+            public void firePropertyChanged() {
+                assertEquals(exercises.get(0).getName(), state.getExercises().get(0).getName());
+                assertEquals(state.getExercisesDisplay()[0][0], "test name");
+                assertEquals(state.getExercisesDisplay()[0][2], "test equipment");
+            }
+        };
+
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        LookUpOutputBoundary presenter = new LookUpPresenter(viewManagerModel, lookUpViewModel);
+
+        presenter.prepareSuccessView(new LookUpOutputData(exercises, false));
+    }
+
+    @Test
+    void prepareFailView() {
+        LookUpViewModel lookUpViewModel = new LookUpViewModel("look up exercise") {
+            private LookUpState state = new LookUpState();
+
+            public LookUpState getState() { return state; }
+
+            public void firePropertyChanged() {
+                assertEquals("look up fail", state.getExercisesError());
+            }
+        };
+
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        LookUpOutputBoundary presenter = new LookUpPresenter(viewManagerModel, lookUpViewModel);
+
+        presenter.prepareFailView("look up fail");
+    }
 }
