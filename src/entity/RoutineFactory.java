d3b79dcdbc72ebb5508fe3879e6833c67c2e1b7e
lisa <lis.ye@mail.utoronto.ca>
2023-12-03 13:36:56 -0500

    Modification of create to static

--- src/entity/RoutineFactory.java
+++ src/entity/RoutineFactory.java
@@ -2,7 +2,7 @@ package entity;
 
 
 public class RoutineFactory {
-    public Routine create(String routineName) {
+    public static Routine create(String routineName) {
         return new Routine(routineName);
     }
 }
