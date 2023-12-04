6c240db09d35efbab1dbc8186f53d86c12bd07a5
dhwanikj <dhwani.jain@mail.utoronto.ca>
2023-12-04 18:22:49 -0500

    just commiting so program can compile

--- src/interface_adapter/add_routine/AddRoutinePresenter.java
+++ src/interface_adapter/add_routine/AddRoutinePresenter.java
@@ -1,11 +1,22 @@
 package interface_adapter.add_routine;
 
 import use_case.add_routine.AddRoutineOutputBoundary;
+import use_case.add_routine.AddRoutineOutputData;
 
 public class AddRoutinePresenter implements AddRoutineOutputBoundary {
     private final AddRoutineViewModel addRoutineViewModel;
 
     public AddRoutinePresenter(AddRoutineViewModel addRoutineViewModel) {
         this.addRoutineViewModel = addRoutineViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(AddRoutineOutputData addRoutineOutputData) {
+
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+
     }
 }
