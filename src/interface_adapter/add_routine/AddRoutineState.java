651dfa0f6c3c748a6308851ac7814fb11d2294b1
cindxyc <chencindy.1206@gmail.com>
2023-12-04 13:20:18 -0500

    little changes to Add Routine State

--- src/interface_adapter/add_routine/AddRoutineState.java
+++ src/interface_adapter/add_routine/AddRoutineState.java
@@ -1,2 +1,32 @@
-package interface_adapter.add_routine;public class AddRoutineState {
+package interface_adapter.add_routine;
+
+
+public class AddRoutineState {
+    private String routineName = "";
+    private String routineNameError = null;
+
+
+    public AddRoutineState(AddRoutineState copy) {
+        routineName = copy.routineName;
+        routineNameError = copy.routineNameError;
+    }
+
+    public AddRoutineState() {
+    }
+    public String getRoutineName(){
+        return routineName;
+    }
+    public String getRoutineNameError() {
+        return routineNameError;
+    }
+    public void setRoutineName(String routineName) {
+        this.routineName = routineName;
+    }
+    public void setRoutineNameError(String routineNameError) {
+        this.routineNameError = routineNameError;
+    }
+    @Override
+    public String toString() {
+        return "AddRoutineState{" + "routinename = " + routineName + "}";
+    }
 }
