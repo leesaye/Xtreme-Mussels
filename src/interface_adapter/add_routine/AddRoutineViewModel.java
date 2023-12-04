75d506606a92951290e8fe66f642c96debfd0ad1
cindxyc <chencindy.1206@gmail.com>
2023-12-04 13:15:12 -0500

    implemented Add Routine View Model

--- src/interface_adapter/add_routine/AddRoutineViewModel.java
+++ src/interface_adapter/add_routine/AddRoutineViewModel.java
@@ -1,10 +1,25 @@
 package interface_adapter.add_routine;
 
-public class AddRoutineViewModel {
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class AddRoutineViewModel extends ViewModel {
+    public static String Add_ROUTINE_BUTTON_LABEL = "Add Routine";
     private AddRoutineState state = new AddRoutineState();
 
-    public AddRoutineViewModel() {
-        super();
+    public AddRoutineViewModel(){ super("AddRoutine"); }
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    // Add Routine Presenter calls the ViewModel
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
     }
 
     public void setState(AddRoutineState state) {
---
+++ test/interface_adapter/add_routine/AddRoutinePresenterTest.java
@@ -0,0 +1,2 @@
+package interface_adapter.add_routine;public class AddRoutinePresenterTest {
+}
