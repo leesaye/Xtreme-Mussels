a5628d2eb9f5fb7561a42ef23f961057e6b05d9d
cindxyc <chencindy.1206@gmail.com>
2023-12-04 17:55:09 -0500

    added and wroted tests for AddRoutine Controller and Presenter

--- test/interface_adapter/add_routine/AddRoutineControllerTest.java
+++ test/interface_adapter/add_routine/AddRoutineControllerTest.java
@@ -1,2 +1,26 @@
-package interface_adapter.add_routine;public class AddRoutineControllerTest {
+package interface_adapter.add_routine;
+
+import org.junit.jupiter.api.Test;
+import use_case.add_routine.AddRoutineInputBoundary;
+import use_case.add_routine.AddRoutineInputData;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AddRoutineControllerTest {
+
+    // TODO: Test this!
+    @Test
+    void test() {
+        AddRoutineInputBoundary addRoutineInteractor = new AddRoutineInputBoundary() {
+
+            @Override
+            public void execute(AddRoutineInputData addRoutineInputData) {
+                assertEquals("routine1", addRoutineInputData.getRoutineName());
+            }
+        };
+
+        AddRoutineController addRoutineController = new AddRoutineController(addRoutineInteractor);
+
+        addRoutineController.execute("routine1");
+    }
 }
