acd71dea2aa69ab34e16e2f848e6b9ae7ab29715
dhwanikj <dhwani.jain@mail.utoronto.ca>
2023-12-04 02:50:34 -0500

    new Main View Model

--- src/interface_adapter/MainViewModel.java
+++ src/interface_adapter/MainViewModel.java
@@ -1,2 +1,25 @@
-package interface_adapter;public class MainViewModel {
+package interface_adapter;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class MainViewModel extends ViewModel{
+    public static final String TITLE_LABEL = "Main View";
+    public static final String LOOKUP_LABEL = "Look Up Exercises";
+    public static final String VIEWALL_LABEL = "View Routines";
+
+    public MainViewModel(String viewName) {
+        super(viewName);
+    }
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    @Override
+    public void firePropertyChanged() {
+
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+//        support.addPropertyChangeListener(listener);
+    }
 }
