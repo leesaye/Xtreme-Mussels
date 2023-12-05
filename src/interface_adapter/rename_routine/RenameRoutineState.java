d60393ce25fa05bb962f70f946670d27f1cbf2ae
DaveZombee <20luky301@gmail.com>
2023-12-05 00:24:59 -0500

    Deleted redundant methods

--- src/interface_adapter/rename_routine/RenameRoutineState.java
+++ src/interface_adapter/rename_routine/RenameRoutineState.java
@@ -4,22 +4,12 @@ public class RenameRoutineState {
 
     private String name;
 
-    private int id;
-
-    private String nameError;
-
     public RenameRoutineState() {
     }
 
-    public int getId() { return id; }
 
     public String getName() { return name; }
 
-    public String getNameError() { return nameError; }
-
-    public void setId(int id) { this.id = id; }
-
     public void setName(String name) { this.name = name; }
 
-    public void setNameError(String nameError) { this.nameError = nameError; }
 }
