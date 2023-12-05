fcfc0656436f73784084f40f683934425379f6c7
cindxyc <chencindy.1206@gmail.com>
2023-12-05 00:10:14 -0500

    final presenter and AddRoutineState

--- src/interface_adapter/add_routine/AddRoutineState.java
+++ src/interface_adapter/add_routine/AddRoutineState.java
@@ -1,15 +1,20 @@
 package interface_adapter.add_routine;
 
+import entity.Exercise;
+import entity.Routine;
+
+import java.util.ArrayList;
 
 public class AddRoutineState {
     private String routineName = "";
     private String routineNameError = null;
+    private Routine routine = null;
+    private String[][] exercisesDisplay;
 
-
-    public AddRoutineState(AddRoutineState copy) {
-        routineName = copy.routineName;
-        routineNameError = copy.routineNameError;
-    }
+//    public AddRoutineState(AddRoutineState copy) {
+//        routineName = copy.routineName;
+//        routineNameError = copy.routineNameError;
+//    }
 
     public AddRoutineState() {
     }
@@ -25,8 +30,20 @@ public class AddRoutineState {
     public void setRoutineNameError(String routineNameError) {
         this.routineNameError = routineNameError;
     }
-    @Override
-    public String toString() {
-        return "AddRoutineState{" + "routinename = " + routineName + "}";
+    public void setExercisesDisplay() {
+        this.exercisesDisplay = this.toStringArray(routine, routine.getExercisesList());
+    }
+
+    public String[][] toStringArray(Routine routine, ArrayList<Exercise> exercise) {
+        if (exercise.isEmpty()) {
+            return new String[][]{{routine.getRoutineName()}};
+        }
+
+        String[][] display = new String[exercise.size()][4];
+        for (int i = 0; i < exercise.size(); i++) {
+            String [] row = {routine.getRoutineName(), exercise.get(i).getTarget(), exercise.get(i).getName()};
+            display[i] = row;
+        }
+        return display;
     }
 }
